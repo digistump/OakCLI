931a21702a290a414f14e2bb7d910325875a106f
Erik Tylek Kettenburg <digistump@gmail.com>
2016-02-23 11:37:29 -0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -3,5 +3,6 @@ source ./version.txt
 npm install
 mkdir -p output/$1/$VERSION
 nexe -i ./oak.js -o ./output/$1/$VERSION/oak
-zip oak-$VERSION-$1.zip ./output/$1/$VERSION
+chmod 0777 ./output/$1/$VERSION/oak
+tar -zcvf oak-$VERSION-$1.tar.gz ./output/$1/$VERSION
 
