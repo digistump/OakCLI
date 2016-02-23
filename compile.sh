73176fb056f72cbde9e0015fe38c732afe15fa52
Erik Tylek Kettenburg <digistump@gmail.com>
2016-02-23 11:36:09 -0800

    grab version from file and set dirs for

--- compile.sh
+++ compile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+source ./version.txt
 npm install
-mkdir -p output/$1
-nexe -i ./oak.js -o ./output/$1/oak-$1
-zip oak-$1.zip ./output/$1/oak-$1
+mkdir -p output/$1/$VERSION
+nexe -i ./oak.js -o ./output/$1/$VERSION/oak
+zip oak-$VERSION-$1.zip ./output/$1/$VERSION
 
